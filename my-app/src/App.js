943b66fa74b6aaa46f4441e6a2c5822925a69ceb
Federico Lencina <federiconahuellencina@gmail.com>
2021-11-26 19:18:00 -0300

    Se agregan los compononentes correspondientes a paginas y la navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,21 @@
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritePage from './pages/Favorite';
+import MainNavigation from './components/layout/MainNavigation';
+
+
 function App() {
   return (
-    <div >
-      
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />}/>
+        <Route path='/new-meetup' element={<NewMeetupPage />}/>
+        <Route path='/favorites' element ={<FavoritePage />}/>
+      </Routes>
     </div>
   );
 }
