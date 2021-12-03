e10951fe91fdbfaecc7d23905c8c5545f8a2fef3
Federico Lencina <federiconahuellencina@gmail.com>
2021-12-03 19:14:05 -0300

    se agregan las funciones fetch para pedir peticiones a la base de datos creada en firebase. Solucionar que se muestren los meetups en pantalla main

--- my-app/src/pages/NewMeetup.js
+++ my-app/src/pages/NewMeetup.js
@@ -1,9 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage(){
+    const navigate = useNavigate();
+
+function addMeetupHandler(meetupData){
+    fetch(
+        'https://reactapp-68feb-default-rtdb.firebaseio.com/meetup.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    ).then(()=>{
+        navigate('/');
+    }); 
+}
+
     return <section>
     <h1>Add new Meetup</h1> 
-    <NewMeetupForm />   
+    <NewMeetupForm onAddMeetup={addMeetupHandler}/>   
     </section>;
 }
 
